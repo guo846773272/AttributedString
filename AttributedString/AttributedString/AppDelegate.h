2fc52212206cc513e86d3ba802279a2350159251
GMY <846773272@qq.com>
2018-01-21 16:02:23 +0800

    AttributedString

---
+++ AttributedString/AttributedString/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AttributedString
+//
+//  Created by GMY on 2018/1/21.
+//  Copyright © 2018年 GMY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AttributedString/AttributedString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttributedString
+//
+//  Created by GMY on 2018/1/21.
+//  Copyright © 2018年 GMY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
